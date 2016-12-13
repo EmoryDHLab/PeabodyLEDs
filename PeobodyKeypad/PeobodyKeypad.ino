00c11b6c8726024ea4a0fb797e6c614fa1288097
Slava <skucheryavykh3@gatech.edu>
2016-12-13 17:35:53 -0500

    first milestone

--- PeobodyKeypad/PeobodyKeypad.ino
+++ PeobodyKeypad/PeobodyKeypad.ino
@@ -1,17 +1,28 @@
 #include <Keypad.h>
 
-const byte ROWS = 3; // Four rows
-const byte COLS = 3; // Three columns
+int const ROWS = 8; // Four rows
+int const COLS = 15; // Three columns
+
+/*
 // Define the Keymap
 char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'}
 };
+*/
+
+
+
+
+
+int keys[ROWS][COLS];
+
+
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 9, 8, 7 };
+byte rowPins[ROWS] = {6, 7, 8, 9, 10, 11, 12, 13};
 // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 12, 11, 10 }; 
+byte colPins[COLS] = {23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37}; 
 
 // Create the Keypad
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
@@ -20,6 +31,7 @@ Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup()
 {
+  mKeymap(ROWS, COLS);
   pinMode(ledpin,OUTPUT);
   digitalWrite(ledpin, HIGH);
   Serial.begin(9600);
@@ -27,8 +39,22 @@ void setup()
 
 void loop()
 {
-  char key = kpd.getKey();
+  int key = kpd.getKey();
   if(key){  // Check for a valid key.
     Serial.println(key);
+  }
+}
+
+void mKeymap(int m, int n)
+{
+  //int keys[m][n];
+  int i,j, k;
+  for(i = 0; i < m; i++)
+  {
+    for(j = 0; j < n; j++)
+    {
+      keys[i][j] = k;
+      k++;
+    }
   }
 }
